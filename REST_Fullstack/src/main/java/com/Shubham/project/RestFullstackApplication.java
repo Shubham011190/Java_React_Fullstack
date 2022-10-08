6ee162174163151c223f03f865481cb3d5700bd0
Shubham Singhal <shubham.011190@gmail.com>
2022-10-08 15:07:35 +0530

    initial commit

---
+++ REST_Fullstack/src/main/java/com/Shubham/project/RestFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.Shubham.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullstackApplication.class, args);
+	}
+
+}
